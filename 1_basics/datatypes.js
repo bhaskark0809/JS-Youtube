50d6766deb950524b617cb77e22a12cf7ee3c8e3
Bhaskar <hacker1204136@gmail.com>
2025-07-29 11:26:42 +0530

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // Treat all JS code as new version
+let name = "bhaskar"; // String       
+let age = 25; //
+let isStudent = false; // Boolean
+let state; // Undefined
+         //primitives
+//number =>2 to pwr(53) 
+// BigInt => 2 to pwr(64)
+// sting => ""
+//boolean => true/false
+// undefined => variable is declared but not assigned a value
+// null => empty value
+// Symbol => unique value
+           // non-primitives
+
+//object
+// array
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (this is a known bug in JS)
