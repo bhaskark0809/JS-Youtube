8b2f082c3fa7a28cfa09f3f14db00a84b197c09c
Bhaskar <hacker1204136@gmail.com>
2025-07-29 10:34:06 +0530

    variable

---
+++ 1_basics/variables.js
@@ -0,0 +1,6 @@
+const userId=10;
+let name="bhaskar";
+var age=25;
+city = "hyderabad";
+console.table([userId, name, age, city]);
+console.log(`User ID: ${userId}, Name: ${name}, Age: ${age}, City: ${city}`);
