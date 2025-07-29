97972015a760666c2f950dbc7b0d1bd3025b977b
Bhaskar <hacker1204136@gmail.com>
2025-07-29 10:45:06 +0530

    variables

--- 1_basics/variables.js
+++ 1_basics/variables.js
@@ -1,6 +1,8 @@
 const userId=10;
 let name="bhaskar";
-var age=25;
+var age=24;
 city = "hyderabad";
+name="bhaskar rao";
+var age=25; // redeclaring with var
 console.table([userId, name, age, city]);
 console.log(`User ID: ${userId}, Name: ${name}, Age: ${age}, City: ${city}`);
